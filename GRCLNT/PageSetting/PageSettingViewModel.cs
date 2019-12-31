f6b902db70406ee9e4db00dcc7c6b5b94eafe3cb
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-12-31 21:40:54 +0800

    setting

--- GRCLNT/PageSetting/PageSettingViewModel.cs
+++ GRCLNT/PageSetting/PageSettingViewModel.cs
@@ -1,4 +1,7 @@
-﻿using Stylet;
+﻿using GRModel;
+using GRSocket;
+using GRUtil;
+using Stylet;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,10 +20,31 @@ namespace GRCLNT
 
         #region SocketHandler
 
+        private void GRSocketHandler_edtUser(RES_STATE state)
+        {
+            GRSocketHandler.edtUser -= GRSocketHandler_edtUser;
+            switch (state)
+            {
+                case RES_STATE.OK:
+                    wndMainVM.messageQueueBd.Enqueue("修改用户信息成功");
+                    break;
+                case RES_STATE.FAILED:
+                    userBd = C_RT.user;
+                    wndMainVM.messageQueueBd.Enqueue("修改用户信息失败");
+                    break;
+                default:
+                    break;
+            }
+        }
+
         #endregion SocketHandler
 
         #region Bindings
         public int pageIndexBd { get; set; } = 0;
+        public C_User userBd { get; set; } = C_RT.user;
+
+        public string edtTelBd { get; set; }
+        public string edtEmailBd { get; set; }
 
         #endregion Bindings
 
@@ -29,6 +53,22 @@ namespace GRCLNT
         {
             wndMainVM.SelectPage((E_Page)Enum.Parse(typeof(E_Page), cmdPara, true));
         }
+
+        public void btnEdtUserCmd()
+        {
+            GRSocketHandler.edtUser += GRSocketHandler_edtUser;
+            userBd.Tel = edtTelBd;
+            userBd.Email = edtEmailBd;
+            GRSocketAPI.EdtUser(userBd);
+        }
+
+        public void btnJumpToEdtUserInfoCmd()
+        {
+            SelectPageCmd("Setting_EdtUserInfo");
+            edtTelBd = userBd.Tel;
+            edtEmailBd = userBd.Email;
+        }
+
         #endregion Actions
     }
 }
