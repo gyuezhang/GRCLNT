0193731c9d4cd7e5a93dfbc1289dd640af9656ad
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2019-12-31 18:04:28 +0800

    add sub pages

---
+++ GRCLNT/PageEntWell/PageEntWell_AddAuto_View.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GRCLNT.PageEntWell
+{
+    /// <summary>
+    /// PageEntWell_AddAuto_View.xaml 的交互逻辑
+    /// </summary>
+    public partial class PageEntWell_AddAuto_View : UserControl
+    {
+        public PageEntWell_AddAuto_View()
+        {
+            InitializeComponent();
+        }
+    }
+}
