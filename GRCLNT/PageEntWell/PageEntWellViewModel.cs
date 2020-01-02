6160cc2f476e01132c2f0bfd2522ac4a85896f3a
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2020-01-02 08:47:57 +0800

    ent para

--- GRCLNT/PageEntWell/PageEntWellViewModel.cs
+++ GRCLNT/PageEntWell/PageEntWellViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using BruTile.Predefined;
+using GRModel;
+using GRSocket;
+using GRUtil;
 using Mapsui.Layers;
 using Mapsui.Projection;
 using Mapsui.UI.Wpf;
@@ -16,18 +19,83 @@ namespace GRCLNT
         public PageEntWellViewModel(WndMainViewModel _wndMainVM)
         {
             wndMainVM = _wndMainVM;
-            InitMap();
+
+            GRSocketHandler.getEntWellParas += GRSocketHandler_getEntWellParas;
+            isWaitingForRefreshParas = true;
+            GRSocketAPI.GetEntWellParas();
         }
+
         private WndMainViewModel wndMainVM { get; set; }
 
         #region SocketHandler
+
+        private void GRSocketHandler_getEntWellParas(RES_STATE state, C_WellParas wps)
+        {
+            GRSocketHandler.getEntWellParas -= GRSocketHandler_getEntWellParas;
+            switch (state)
+            {
+                case RES_STATE.OK:
+                    wpBd = new C_WellParas(wps.All);
+                    break;
+                case RES_STATE.FAILED:
+                    wndMainVM.messageQueueBd.Enqueue("获取企业井参数失败");
+                    break;
+                default:
+                    break;
+            }
+            isWaitingForRefreshParas = false;
+        }
+
+        private void GRSocketHandler_addEntWellPara(RES_STATE state)
+        {
+            GRSocketHandler.addEntWellPara -= GRSocketHandler_addEntWellPara;
+            switch (state)
+            {
+                case RES_STATE.OK:
+                    wndMainVM.messageQueueBd.Enqueue("添加企业井参数成功");
+                    GRSocketHandler.getEntWellParas += GRSocketHandler_getEntWellParas;
+                    isWaitingForRefreshParas = true;
+                    GRSocketAPI.GetEntWellParas();
+                    break;
+                case RES_STATE.FAILED:
+                    wndMainVM.messageQueueBd.Enqueue("添加企业井参数失败");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void GRSocketHandler_delEntWellPara(RES_STATE state)
+        {
+            GRSocketHandler.delEntWellPara -= GRSocketHandler_delEntWellPara; ;
+            switch (state)
+            {
+                case RES_STATE.OK:
+                    wndMainVM.messageQueueBd.Enqueue("删除企业井参数成功");
+                    GRSocketHandler.getEntWellParas += GRSocketHandler_getEntWellParas;
+                    isWaitingForRefreshParas = true;
+                    GRSocketAPI.GetEntWellParas();
+                    break;
+                case RES_STATE.FAILED:
+                    wndMainVM.messageQueueBd.Enqueue("删除企业井参数失败");
+                    break;
+                default:
+                    break;
+            }
+        }
 
         #endregion SocketHandler
 
         #region Bindings
         public int pageIndexBd { get; set; } = 0;
-        public MapControl map { get; set; } = new MapControl();
 
+        //parasetting
+        public string strPsLocBd { get; set; }
+        public string strPsUnitCatBd { get; set; }
+        public string strPsTubeMatBd { get; set; }
+        public string strPsPumpModelBd { get; set; }
+        public string strPsUseForBd { get; set; }
+        public C_WellParas wpBd { get; set; } = new C_WellParas();
         #endregion Bindings
 
         #region Actions
@@ -35,17 +103,76 @@ namespace GRCLNT
         {
             wndMainVM.SelectPage((E_Page)Enum.Parse(typeof(E_Page), cmdPara, true));
         }
-        #endregion Actions
 
-        public void InitMap()
+
+        //parasetting
+        public bool CanpsLocAddCmd => (strPsLocBd != null && !isWaitingForRefreshParas);
+        public void psLocAddCmd()
         {
-            map.Map.Layers.Add(new TileLayer(KnownTileSources.Create()));
-            //map.Map.Layers.Add(CreateWellLayer());
-
-            var centerOfBD = new Mapsui.Geometries.Point(117.309716, 39.717173);
-            var sphericalMercatorCoordinate = SphericalMercator.FromLonLat(centerOfBD.X, centerOfBD.Y);
-            map.Map.Home = n => n.NavigateTo(sphericalMercatorCoordinate, map.Map.Resolutions[12]);
-
+            GRSocketHandler.addEntWellPara += GRSocketHandler_addEntWellPara;
+            GRSocketAPI.AddEntWellPara(new C_WellPara(E_WellParaType.Loc, strPsLocBd));
         }
+
+        public bool CanpsLocDelCmd => (wpBd.LocIndex != null && wpBd.LocIndex.Value != "" && wpBd.LocIndex.Value != null && !isWaitingForRefreshParas);
+        public void psLocDelCmd()
+        {
+            GRSocketHandler.delEntWellPara += GRSocketHandler_delEntWellPara; ;
+            GRSocketAPI.DelEntWellPara(wpBd.LocIndex);
+        }
+
+        public bool CanpsUnitCatAddCmd => (strPsUnitCatBd != null && !isWaitingForRefreshParas);
+        public void psUnitCatAddCmd()
+        {
+            GRSocketHandler.addEntWellPara += GRSocketHandler_addEntWellPara;
+            GRSocketAPI.AddEntWellPara(new C_WellPara(E_WellParaType.UnitCat, strPsUnitCatBd));
+        }
+        public bool CanppsUnitCatDelCmd => (wpBd.UnitCatIndex != null && wpBd.UnitCatIndex.Value != "" && wpBd.UnitCatIndex.Value != null && !isWaitingForRefreshParas);
+        public void psUnitCatDelCmd()
+        {
+            GRSocketHandler.delEntWellPara += GRSocketHandler_delEntWellPara; ;
+            GRSocketAPI.DelEntWellPara(wpBd.UnitCatIndex);
+        }
+
+        public bool CanpsTubeMatAddCmd => (strPsTubeMatBd != null && !isWaitingForRefreshParas);
+        public void psTubeMatAddCmd()
+        {
+            GRSocketHandler.addEntWellPara += GRSocketHandler_addEntWellPara;
+            GRSocketAPI.AddEntWellPara(new C_WellPara(E_WellParaType.TubeMat, strPsTubeMatBd));
+        }
+        public bool CanpsTubeMatDelCmd => (wpBd.TubeMatIndex != null && wpBd.TubeMatIndex.Value != "" && wpBd.TubeMatIndex.Value != null && !isWaitingForRefreshParas);
+        public void psTubeMatDelCmd()
+        {
+            GRSocketHandler.delEntWellPara += GRSocketHandler_delEntWellPara; ;
+            GRSocketAPI.DelEntWellPara(wpBd.TubeMatIndex);
+        }
+
+        public bool CanpsPumpModelAddCmd => (strPsPumpModelBd != null && !isWaitingForRefreshParas);
+        public void psPumpModelAddCmd()
+        {
+            GRSocketHandler.addEntWellPara += GRSocketHandler_addEntWellPara;
+            GRSocketAPI.AddEntWellPara(new C_WellPara(E_WellParaType.PumpModel, strPsPumpModelBd));
+        }
+        public bool CanpsPumpModelDelCmd => (wpBd.PumpModelIndex != null && wpBd.PumpModelIndex.Value != "" && wpBd.PumpModelIndex.Value != null && !isWaitingForRefreshParas);
+        public void psPumpModelDelCmd()
+        {
+            GRSocketHandler.delEntWellPara += GRSocketHandler_delEntWellPara; ;
+            GRSocketAPI.DelEntWellPara(wpBd.PumpModelIndex);
+        }
+
+        public bool CanpsUseForAddCmd => (strPsUseForBd != null && !isWaitingForRefreshParas);
+        public void psUseForAddCmd()
+        {
+            GRSocketHandler.addEntWellPara += GRSocketHandler_addEntWellPara;
+            GRSocketAPI.AddEntWellPara(new C_WellPara(E_WellParaType.UseFor, strPsUseForBd));
+        }
+        public bool CanpsUseForDelCmd => (wpBd.UseForIndex != null && wpBd.UseForIndex.Value != "" && wpBd.UseForIndex.Value != null && !isWaitingForRefreshParas);
+        public void psUseForDelCmd()
+        {
+            GRSocketHandler.delEntWellPara += GRSocketHandler_delEntWellPara; ;
+            GRSocketAPI.DelEntWellPara(wpBd.UseForIndex);
+        }
+        #endregion Actions
+        public bool isWaitingForRefreshParas { get; set; } = false;
+
     }
 }
