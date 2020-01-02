b3610b6c448af94c2226d0276773f602257c4e97
gyuezhang <47130297+gyuezhang@users.noreply.github.com>
2020-01-02 09:01:48 +0800

    web

--- GRCLNT/PageEntWell/PageEntWell_FetchWaterId.xaml.cs
+++ GRCLNT/PageEntWell/PageEntWell_FetchWaterId.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,5 +25,27 @@ namespace GRCLNT
         {
             InitializeComponent();
         }
+
+        private void WebBrowser_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            SetWebBrowserSilent(sender as WebBrowser, true);
+        }
+
+        /// <summary>
+        /// 设置浏览器静默，不弹错误提示框
+        /// </summary>
+        /// <param name="webBrowser">要设置的WebBrowser控件浏览器</param>
+        /// <param name="silent">是否静默</param>
+        private void SetWebBrowserSilent(WebBrowser webBrowser, bool silent)
+        {
+            FieldInfo fi = typeof(WebBrowser).GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fi != null)
+            {
+                object browser = fi.GetValue(webBrowser);
+                if (browser != null)
+                    browser.GetType().InvokeMember("Silent", BindingFlags.SetProperty, null, browser, new object[] { silent });
+            }
+        }
+
     }
 }
